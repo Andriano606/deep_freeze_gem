bd0e4c575c92688fccc62c33b447ce3d640b8789
Andrii Kuluiev <andreykuluev@gmail.com>
2025-01-12 00:08:03 +0200

    asd

--- lib/deep_freeze.rb
+++ lib/deep_freeze.rb
@@ -3,6 +3,22 @@
 require_relative "deep_freeze/version"
 
 module DeepFreeze
-  class Error < StandardError; end
-  # Your code goes here...
+  def deep_freeze(obj)
+    case obj
+    when String , Symbol, Numeric, TrueClass, FalseClass
+      obj.freeze
+    when Hash
+      obj.each do |key, value|
+        deep_freeze(key)
+        deep_freeze(value)
+      end
+      obj.freeze
+    when Array
+      obj.each do |element|
+        deep_freeze(element)
+      end
+    else
+      raise 'Invalid object'
+    end
+  end
 end
